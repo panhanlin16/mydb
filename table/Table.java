182001586fa30401b00e5b319f02b9338c0286ab
panhanlin16 <40332634+panhanlin16@users.noreply.github.com>
2019-05-20 20:29:10 +0800

    Update Table.java

--- table/Table.java
+++ table/Table.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Table implements Serializable{
+public class Table implements Serializable
+{
 	 
 	private static final long serialVersionUID = 1L;
 
